ce443e476c7515792a7a462dc41391300b0de804
yakshna-technologies <78855806+yakshna-technologies@users.noreply.github.com>
2021-03-09 15:00:47 +0530

    Delete run.php

--- run.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'src/Calculator.php';
-
-echo Calculator::add(0, 1);
-echo Calculator::subtract(4, 2);
-echo Calculator::multiply(3, 1);
-echo Calculator::divide(16, 4);
-echo "\n";
